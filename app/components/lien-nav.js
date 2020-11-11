ed34ca68bbf9ad4f66cfd90f383644ca4774d690
jeremieBughin <jeremie.bughin@gmail.com>
2020-11-11 23:21:56 +0100

    1day last commit

--- app/components/lien-nav.js
+++ app/components/lien-nav.js
@@ -1,12 +1,25 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 export default class LienNavComponent extends Component {
-    @action handleInteraction(e) { 
-        e.preventDefault();
-        this.args.action();
-      }
+    @tracked onButton = this.args.state ;
+    //@tracked hoverOff = this.args.hoverOff ; 
 
-    @action handlerHover(){
-      console.log(this.args.value)
+    @action
+    handleInteraction() {
+      console.log(this.args.hoverOff)
     }
+    @action handlerHoverOn(){
+      if(this.args.hoverOff == false){
+        this.onButton = false ; 
+        console.log(this.onButton) 
+      }
+           
     }
+    @action handlerHoverOff(){
+      if(this.args.hoverOff == false){
+        this.onButton = true ; 
+        console.log(this.onButton)
+      }  
+    }
+}
--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,13 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+
 export default class NavBarComponent extends Component {
 
-    @action
-  showPerson(person) {
-    alert(`The person's name is ${person}!`);
-  }
-  @action 
-  hoverButton(){
-      console.log(this.args);
-  }
 }
